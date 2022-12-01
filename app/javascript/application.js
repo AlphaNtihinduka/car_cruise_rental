9f4faa0089e3bae38852818897884a513e080e26
Titilope Apuabi <tyaps002@gmail.com>
2022-12-01 23:13:59 +0100

    Add more configurations

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,12 @@
 // Entry point for the build script in your package.json
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+function App() {
+  return (<h1>Hello World!</h1>);
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root'),
+);
