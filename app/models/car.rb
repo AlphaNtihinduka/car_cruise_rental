05fa16f6c1d76ee69382d4db004d6ac60681332c
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-12-03 00:57:17 +0200

    Add seed files and routing

--- app/models/car.rb
+++
@@ -1,4 +0,0 @@
-class Car < ApplicationRecord
-  belongs_to :user
-  has_many :reservations
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'root#index'
+ devise_for :users
+
+  namespace :api do
+    namespace :v1 do
+       resources :cars
+    end  
+  end
+
+  root "root#index"
+
 end
