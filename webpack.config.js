c2af6d6333d7bb4ccbffdea3d4b6e317fa71f91c
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-12-10 02:08:55 +0300

    Fix linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -16,16 +16,6 @@ module.exports = {
       },
     ],
   },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        options: { presets: ['@babel/env', '@babel/preset-react'] },
-      },
-    ],
-  },
   output: {
     filename: '[name].js',
     sourceMapFilename: '[name].js.map',
