e37545c5e23bc041b565aae116383d761b4f7e2d
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-12-10 02:17:32 +0300

    Fix eslint errors

--- app/javascript/redux/reducers/carReducer.js
+++ app/javascript/redux/reducers/carReducer.js
@@ -1,4 +1,4 @@
-import { ActionTypes } from '../constants/actionTypes';
+import ActionTypes from '../constants/actionTypes';
 
 const initialState = {
   cars: [],
--- app/javascript/redux/reservation_actions/reservation_actions.js
+++ app/javascript/redux/reservation_actions/reservation_actions.js
@@ -1,4 +1,4 @@
-import { ActionTypes } from '../constants/actionTypes';
+import ActionTypes from "../constants/actionTypes";
 
 const setReservations = (reservations) => ({
   type: ActionTypes.SET_RESERVATIONS,
