c8e4f08794afe45d2abe6bb4280f261438859487
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-12-10 01:18:22 +0300

    Create car actions

---
+++ app/javascript/redux/carActions/carActions.js
@@ -0,0 +1,15 @@
+import { ActionTypes } from "../constants/actionTypes"
+
+export const setCars = (cars) => {
+    return {
+        type: ActionTypes.SET_CARS,
+        payload: cars
+    }
+}
+
+export const selectedCar = (car) => {
+    return {
+        type: ActionTypes.SELECTED_CAR,
+        payload: car
+    }
+}
