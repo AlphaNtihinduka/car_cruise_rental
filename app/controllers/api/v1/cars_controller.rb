2c5559632fc8b97efe08f1cce3a256c80c0e6d61
Titilope Apuabi <tyaps002@gmail.com>
2022-12-09 23:33:45 +0100

    Implement functionalities for navigation

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,11 +1,10 @@
 class Api::V1::CarsController < ApplicationController
- 
   def index
     @cars = Car.all.order(created_at: :desc)
     render json: @cars
   end
 
-   # GET /cars/1
+  # GET /cars/1
   def show
     @car = Car.find(params[:id])
     render json: @car
@@ -16,11 +15,8 @@ class Api::V1::CarsController < ApplicationController
     # @user = User.find(params[:id])
     @car = Car.new(car_params)
 
-    
     if @car.save
       render json: @car, status: :created
-    elsif 
-      render json: { message: 'You are not an admin user' }
     else
       render json: @car.errors, status: :unprocessable_entity
     end
@@ -39,9 +35,8 @@ class Api::V1::CarsController < ApplicationController
   def destroy
     @car = Car.find(params[:id])
     @car.delete
-   
   end
-  
+
   alias delete destroy
 
   private
@@ -53,7 +48,6 @@ class Api::V1::CarsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def car_params
-    params.require(:car).permit(:image ,:name, :description, :price_per_day, :user_id)
+    params.require(:car).permit(:image, :name, :description, :price_per_day, :user_id)
   end
-
 end
