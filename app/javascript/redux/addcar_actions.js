5b858a2ca59730926530add0c1152a798e935202
Titilope Apuabi <tyaps002@gmail.com>
2022-12-10 22:35:57 +0100

    Update form to state

---
+++ app/javascript/redux/addcar_actions.js
@@ -0,0 +1,11 @@
+// import ActionTypes from '../constants/actionTypes';
+
+// export const setCars = (addcars) => ({
+//   type: ActionTypes.ADD_CAR,
+//   payload: addcars,
+// });
+
+// export const SELECTED_CARS = (car) => ({
+//   type: ActionTypes.SELECTED_CARS,
+//   payload: car,
+// });
