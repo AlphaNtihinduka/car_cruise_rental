042957f51f63c1e81928adb6c34e0d8543249979
Titilope Apuabi <tyaps002@gmail.com>
2022-12-11 22:09:36 +0100

    Implement functionalities for navigation

--- app/javascript/components/navigation/HomePage.js
+++ app/javascript/components/navigation/HomePage.js
@@ -4,19 +4,19 @@ import Socials from './Socials';
 const HomePage = () => {
   const title = ' Car Rental ';
 
-  // return (
-  //   <header className="navbar">
-  //     <div className="title">
-  //       <h1>
-  //         Welcome! to
-  //         {title}
-  //         HomePage
-  //       </h1>
-  //     </div>
-  //     <div>
-  //       <Socials />
-  //     </div>
-  //   </header>
-  // );
+  return (
+    <header className="navbar">
+      {/* <div className="title">
+        <h1>
+          Welcome! to
+          {title}
+          HomePage
+        </h1>
+      </div>
+      <div>
+        <Socials />
+      </div> */}
+    </header>
+  );
 };
 export default HomePage;
