da709b17de96ed5828aa227ea9c3e135b2f0c4b8
Titilope Apuabi <tyaps002@gmail.com>
2022-12-11 22:14:34 +0100

    Implement functionalities for navigation

--- app/javascript/components/navigation/Navigate.js
+++ app/javascript/components/navigation/Navigate.js
@@ -5,11 +5,11 @@ import { Link } from 'react-router-dom';
 const Navigate = () => (
   <div>
     <ul>
-      {/* <li><Link to="/">HomePage</Link></li>
+      <li><Link to="/">HomePage</Link></li>
       <li><Link to="/addcar">AddCar</Link></li>
       {/* <Link to="/reserve-form"><li>#</li></Link>
             <Link to="/my-reservation"><li>#</li></Link> */}
-      <li><Link to="/delete" /></li> */}
+      <li><Link to="/delete" /></li>
     </ul>
     <div>
       {/* <Socials /> */}
