782a702d60e99dd06fcc71c75f0dcab7f136c192
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-12-12 02:37:53 +0200

    Add car components code

--- app/javascript/components/car_lists/CarComponent.js
+++ app/javascript/components/car_lists/CarComponent.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const CarComponent = () => {
   const cars = useSelector((state) => state.allcars.cars);
   const carRender = cars.map((car) => {
-    const {
-      id, name, image, description,
-    } = car;
+    const { id, name, image, description } = car;
     return (
       <div key={id}>
         <Link to={`/car/${id}`}>
-          <div className="image"><img src={image} alt="name" /></div>
+          <div className="image">
+            <img src={image} alt="name" />
+          </div>
           <h2>{name}</h2>
           <h4>{description}</h4>
         </Link>
