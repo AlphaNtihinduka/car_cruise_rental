7ac7e482bed09de1930144459c76567320ebe2fd
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-12-12 07:26:19 +0200

    Add exact path to car details routing

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Route, Routes } from 'react-router';
-import CarListing from './components/car_lists/CarListing';
-import CarDetails from './components/car_detail/CarDetail';
-import '../assets/stylesheets/application.css';
-import Navigate from './components/navigation/Navigation';
+import React from "react";
+import { Route, Routes } from "react-router";
+import CarListing from "./components/car_lists/CarListing";
+import CarDetails from "./components/car_detail/CarDetail";
+import "../assets/stylesheets/application.css";
+import Navigate from "./components/navigation/Navigation";
 
 function App() {
   return (
     <div className="App">
-      <Navigate /> 
+      <Navigate />
       <Routes>
         <Route exact path="/" element={<CarListing />} />
-        <Route path="/car/:car_id" element={<CarDetails />} />
+        <Route exact path="/car/:car_id" element={<CarDetails />} />
       </Routes>
     </div>
   );
