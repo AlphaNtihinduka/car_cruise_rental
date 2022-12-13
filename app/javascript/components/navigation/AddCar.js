c367e4d3ed36665dee955f6c1635f7e40ee8fc7e
Titilope Apuabi <tyaps002@gmail.com>
2022-12-14 00:54:43 +0100

    Add styling

--- app/javascript/components/navigation/AddCar.js
+++ app/javascript/components/navigation/AddCar.js
@@ -44,27 +44,38 @@ const AddCar = () => {
   };
 
   return (
+    <div className="form-container">
     <div className="add-form">
       <h1>Add A New Rental Car</h1>
       <div className="container d-flex justify-content-center align-items-center">
-        <form>
+        <form className='main-form'>
           <div className="form-group">
-            <label htmlFor="name">Name</label>
-            <input type="text" value={name} id="name" onChange={(event) => setname(event.target.value)} />
-            <label htmlFor="user_id">User ID</label>
-            <input type="text" value={user} id="user_id" onChange={(event) => setUser(event.target.value)} />
-            <label htmlFor="price_per_day">Price Per Day</label>
-            <input type="text" value={pricePerDay} id="price_per_day" onChange={(event) => setPricePerDay(event.target.value)} />
-            <label htmlFor="description">Description</label>
-            <input type="text" value={description} id="description" onChange={(event) => setDescription(event.target.value)} />
-            {/* <input type="text" value={image} id="image" onChange={(event) => setImage(event.target.value)} /> */}
+            <div className="nform">
+              <label htmlFor="name">Name</label>
+              <input type="text" value={name} id="name" onChange={(event) => setname(event.target.value)} />
+            </div>
+            <div className="iform">
+              <label htmlFor="user_id">User ID</label>
+              <input type="text" value={user} id="user_id" onChange={(event) => setUser(event.target.value)} />
+            </div>
+            <div className="pform">
+              <label htmlFor="price_per_day">Price Per Day</label>
+              <input type="text" value={pricePerDay} id="price_per_day" onChange={(event) => setPricePerDay(event.target.value)} />
+            </div>
+            <div className="dform">
+              <label htmlFor="description">Description</label>
+              <input type="text" value={description} id="description" onChange={(event) => setDescription(event.target.value)} />
+            </div>
+            <div className='form-image'>
+              <label htmlFor="image">Image</label>
             <Select options={options} onChange={handleSelectChange} />
+            </div>
             <div>
-                <button onClick={handleSubmit} type="button">Submit</button>
+                <button className="btn-form" onClick={handleSubmit} type="button">Submit</button>
             </div>
            </div>
         </form>
-        <div className="container d-grid justify-content-center align-items-center">
+
           {/* <Table className="table d-flex grid-template-column-1fr 1fr">
             <thead>
               <tr>
--- app/javascript/components/navigation/DeleteCar.js
+++ app/javascript/components/navigation/DeleteCar.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { setCars } from '../../redux/carActions/carActions';
 
 
@@ -12,20 +12,15 @@ const DeleteCar = () => {
         .catch((err) => err);
       dispatch(setCars(response.data));
     };
-    const cars = useSelector((state) => state.allcars.cars);
-    const carRender = cars.map((car) => {
-      const {
-        id, name, image, description,
-      } = car;
-    });
+    
 
     const handleDelete = async (id) => {
       const response = await axios
-        .delete(`http://127.0.0.1:5000/api/v1/cars`, { id })
+        .delete(`http://127.0.0.1:5000/api/v1/cars/${id}`)
     };
 
     useEffect(() => {
-      fetchdata();
+      fetchdata()
     }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
     return (
@@ -37,7 +32,7 @@ const DeleteCar = () => {
           </div>
           <h2>{name}</h2>
           <h4>{description}</h4>
-          <button type="button" onClick={() => DeleteCar(id)}>Delete</button>
+          <button type="button" onClick={() => handleDelete(id)}>Delete</button>
         </Link>
       </div>
       </>
