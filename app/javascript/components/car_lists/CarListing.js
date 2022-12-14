52905dd89213bc85e4509277c1a4fad4214a71fe
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-12-14 21:43:10 +0300

    Fix eslint errors

--- app/javascript/components/car_lists/CarListing.js
+++ app/javascript/components/car_lists/CarListing.js
@@ -19,12 +19,12 @@ const CarListing = () => {
 
   return (
     <>
-      <div className='car_list_heading'>
-        <h1 className='latest_model'>The latest car models</h1>
-        <p className='selected_model'>Enjoy the ride with selected model</p>
+      <div className="car_list_heading">
+        <h1 className="latest_model">The latest car models</h1>
+        <p className="selected_model">Enjoy the ride with selected model</p>
 
       </div>
-      
+
       <div className="all_cars_container">
         <CarComponent />
       </div>
