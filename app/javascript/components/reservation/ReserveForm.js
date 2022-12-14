8a1099c89925171b05ba271dcbb81d6c99310855
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-12-14 16:32:11 +0200

    Add new code for reserving car

--- app/javascript/components/reservation/ReserveForm.js
+++ app/javascript/components/reservation/ReserveForm.js
@@ -1,78 +1,134 @@
 import React, { useEffect, useState } from 'react';
-// import Table from 'react-bootstrap/Table';
 import axios from 'axios';
-// import './styles.css';
+import { useParams } from "react-router-dom";
 
 const AddReservation = () => {
-    const allCars = []
+  // Initialize state for car data
+  const [allCars, setAllCars] = useState([]);
+  const [carName, setCarName] = useState("");
+  const [days, setDays] = useState("");
+  const [pickDate, setPickDate] = useState("");
+  const [user_id, setUserId] = useState("");
+  const {car_id } = useParams();
+ const [carId, setCarId] = useState("");
+  // const [car, setCar] = useState();
+
+  // Initialize state for car data
+
+  useEffect(() => {
+    // Use `manageCars` function to fetch car data
     const manageCars = async () => {
-        const responses = await axios
-      .get('http://127.0.0.1:5000/api/v1/cars')
-      .catch((err) => err);
-      console.log("respo",responses.data)
-      responses.data.map((car) => {
-        allCars.push(car)
-        console.log(car)
-      }) 
+      const responses = await axios
+        .get("http://127.0.0.1:5000/api/v1/cars")
+        .catch((err) => err);
+
+      // Update component state with car data
+      setAllCars(responses.data);
+      const car = allCars.find((car) => car.id === car_id);
+    };
+
+    // Call `manageCars` function to fetch data
+    manageCars();
+  }, []);
+  // Empty array means this effect will only run once when the component mounts
+const handleSubmit = async () => {
+  try {
+    // Create reservation object with the input fields values
+    const reservation = {
+      car_id: carId,
+      user_id: user_id,
+      days: days,
+      pick_date: pickDate,
+    };
+
+    // Make a POST request to add the reservation
+    const response = await axios.post(
+      `http://127.0.0.1:5000/api/v1/cars/${car_id}/reservations`,
+      reservation
+    );
+
+    // Handle success or failure
+    if (response.status === 200) {
+      // Success, show a message and reset the form
+      alert("Reservation added successfully!");
+      setCarId("");
+      setUserId("");
+      setDays("");
+      setPickDate("");
+    } else {
+      // Failure, show an error message
+      alert("Error adding reservation: " + response.data.message);
     }
- 
-      manageCars();
-    
-    // console.log("all cars",allCars)
+  } catch (error) {
+    // Handle network errors
+    alert("Error adding reservation: " + error.message);
+  }
+};
 
-    
-
-
-  const [days, setdays] = useState('');
-  const [pickDate, setPickDate] = useState('');
-  const [car_id, setCarId] = useState('');
-  const [user_id, setUserId] = useState('');
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const reservation = {};
-    reservation.days = days;
-    reservation.pick_date = pickDate;
-    reservation.car_id = car_id;
-    reservation.user_id = 1;
-
-    const response = await fetch(`http://127.0.0.1:5000/api/v1/cars/${car_id}/reservations`, {
-      method: 'POST',
-      body: JSON.stringify(reservation),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    console.log(await response.json());
-    window.location.href = "/reservations"
-  };
+  // Rest of your component code goes here
 
   return (
     <div className="add-form">
       <h1>Add A New reservation</h1>
-         
-            <input placeholder='days' type="number" value={days} id="days" onChange={(event) => setdays(event.target.value)} /> <br/>
-         
-            <input placeholder='pick date' type="text" value={pickDate} id="pick_date" onChange={(event) => setPickDate(event.target.value)} /> <br/>
-              
-            <select > 
-            <option>Select the car</option>
-            {console.log("all cars option: ",allCars)}
-               {allCars.map((car) => (          
-                <option key={car.id} value={car.id} onChange={(event) => setCarId(event.target.value)}>
-                  {/* {car.name} */}
-                  {console.log("cars:",car)}
-                </option>
-               ))}
-          </select><br/>
-            {/* <input placeholder='car id' type="number" value={car_id} id="car_id" onChange={(event) => setCarId(event.target.value)} /> <br/> */}
-             
-            <input placeholder='user id' type="number" value={user_id} id="user_id" onChange={(event) => setUserId(event.target.value)} /> <br/>
-    
+
+      <input
+        placeholder="Enter number of days "
+        type="number"
+        value={days}
+        id="days"
+        onChange={(event) => setDays(event.target.value)}
+      />
+      <br />
+      <input
+        placeholder="Enter pick date like YYYY-MM-DD."
+        type="date"
+        value={pickDate}
+        id="pick_date"
+        onChange={(event) => setPickDate(event.target.value)}
+      />
+      <br />
+
+      {/* <select onChange={(event) => setCarId(event.target.value)}>
+        <option>Select the car</option>
+        {allCars.map((car) => (
+          <option key={car.id} value={car.id}>
+            {car.name}
+          </option>
+        ))}
+      </select> */}
+
+      <select type="number">
+        <option>Select the car</option>
+        {allCars.map((car) => (
+          <option
+            key={car.id}
+            value={car.id}
+            onChange={(event) => setCarId(event.target.value)}
+          >
+            {car.name}
+          </option>
+        ))}
+      </select>
+      <br />
+      <input
+        placeholder="user id"
+        type="number"
+        value={user_id}
+        id="user_id"
+        onChange={(event) => setUserId(event.target.value)}
+      />
+      <br />
       <div>
-        <button onClick={handleSubmit} type="button">Submit</button>
+        <button onClick={handleSubmit} type="button">
+          Submit
+        </button>
       </div>
     </div>
   );
 };
-export default AddReservation;
+
+export default AddReservation ;
+
+
+
+
