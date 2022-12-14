ae7b67f21b397648c08fd16638bcb25161d9eb2e
Ntare Guy <gntare2@gmail.com>
2022-12-14 03:47:15 +0200

    Add design and sign up page

--- app/javascript/pages/auth/registration.jsx
+++ app/javascript/pages/auth/registration.jsx
@@ -1,9 +1,99 @@
-import React from 'react'
+/* eslint-disable no-console */
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './auth.css';
 
 function Signup() {
-    return ( 
-        <h1>HEllo this Signup</h1>
-     );
+  const [Username, setUsername] = useState();
+  const [Password, setPassword] = useState();
+  const [Email, setEmail] = useState();
+
+  const sendForm = async () => {
+    const dataform = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        name: Username,
+        email: Email,
+        password: Password,
+      }),
+    };
+    const response = await fetch(
+      'http://127.0.0.1:5000/users/sign_up',
+      dataform,
+    );
+    if (response.ok) {
+      const userData = await response.json();
+      console.log('User Registered', userData);
+      window.location.href = '/car';
+    } else {
+      console.log('Error Registering', response);
+    }
+  };
+
+  return (
+    <div className="container">
+      <form className="container-form" method="POST" onSubmit={sendForm()}>
+        <h2>Sign up</h2>
+
+        <div className="form-input">
+          <input
+            onChange={async (e) => {
+              setUsername(e.target.value);
+              e.preventDefault();
+            }}
+            type="text"
+            id="name"
+            name="name"
+            className=""
+            placeholder="Username"
+            required
+          />
+        </div>
+
+        <div className="form-input">
+          <input
+            onChange={async (e) => {
+              setEmail(e.target.value);
+              e.preventDefault();
+            }}
+            type="text"
+            id="email"
+            name="email"
+            className=""
+            placeholder="email"
+            required
+          />
+        </div>
+
+        <div className="form-input">
+          <input
+            onChange={async (e) => {
+              setPassword(e.target.value);
+              e.preventDefault();
+            }}
+            type="text"
+            id="password"
+            name="password"
+            className=""
+            placeholder="Password"
+            required
+          />
+        </div>
+
+        <p className="form-signup">
+          You have an account
+          <Link to="/" className="signup">Login</Link>
+        </p>
+
+        <div className="">
+          <button type="submit" className="form-button">
+            Sign up
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Signup;
