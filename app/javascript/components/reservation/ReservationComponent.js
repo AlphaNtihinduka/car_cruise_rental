b32c0bd35ad53697fa86ff3c04f8d7833895d36a
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-12-16 02:52:18 +0300

    Styling my reservations

--- app/javascript/components/reservation/ReservationComponent.js
+++ app/javascript/components/reservation/ReservationComponent.js
@@ -4,27 +4,30 @@ import { useSelector } from 'react-redux';
 const ReservationComponent = () => {
   const reservations = useSelector((state) => state.allreservations.reservations);
   const reservationRender = reservations.map((reservation) => ( 
-    <div key={reservation.id} className="reservation">
-        {console.log(reservation.car.name)}
-
+    <div key={reservation.id} className="my_reservation_container">
+      <div className='car_reservation_info_container'>
         <div>
           <div className="reservation_image">
             <img src={reservation.car.image} alt="name" />
           </div>
-          <h3>Car name{reservation.car.name}</h3>
-          <p>Total pricing: ${reservation.car.price_per_day * reservation.days}</p>
+           
         </div>
-      <p>
-        Reservation days: 
-        {reservation.days}
-        {' '}
-        days
-      </p>
-      <p>
-        Pick date:
-        {reservation.pick_date}
-      </p>
-      <button>Cancel reservation</button>
+        <div>
+          <h3>{reservation.car.name}</h3> 
+          <p>
+            Period: {' '}
+            {reservation.days}
+            {' '}
+            days
+          </p>
+          <p>
+            Pick date: {' '}
+            {reservation.pick_date}
+          </p>
+          <p>Total pricing: ${reservation.car.price_per_day * reservation.days}</p>
+         </div>
+      </div>
+      <button className='cancel_btn'>Cancel reservation</button>
     </div>
   ));
   return <>{reservationRender}</>;
